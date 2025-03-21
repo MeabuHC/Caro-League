b1e0eead13d442ad903df19a5dd8566badb95ecc
MeabuHC <woolythesheepnew@gmail.com>
2025-03-21 14:43:50 +0700

    Fix again for deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   host: "0.0.0.0",
-  //   port: process.env.PORT || 8080,
-  // },
-  // mode: "production",
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 8080,
+  },
+  mode: "production",
 });
