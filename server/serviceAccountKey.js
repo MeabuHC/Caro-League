f9830632cc1b9629f7957ee8ed0610f5e3d1b2a5
MeabuHC <woolythesheepnew@gmail.com>
2025-04-08 20:57:48 +0700

    hide and invalidate old apikey for security purpose

--- server/serviceAccountKey.js
+++ server/serviceAccountKey.js
@@ -1,15 +1,13 @@
 export default {
-  type: "service_account",
-  project_id: "caro-league",
-  private_key_id: "af69f1b921ccf17e437f914514a948d9252c2cee",
-  private_key:
-    "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQDXDKt4c7KCEB1d\nPuhTi7NaqX46n9RKTir41TyJOOsu0A3ysp69Yx/TpDMvHjiToqcB+aIc6ofUOpeB\nyKnWcjQo9PX13R/6GoG5TC33nv3dD+tcfgnVyyjl9ymC7tNplYN1YWsnMBxnGNGb\nI3pprYbBCV3W0JVMD41pDt5S6aDxwP/qIP9VF6fY/LWg4O5BqK/c4vkIt64SkpN6\nktVUnzKktCh6OEO2A46r9os0C4eXYb+jWvm8DYijvQVi8edO8eJKcKrPfpjpl64j\nSEOjfRaTpJYvAURe4tUKNQ5APwn0rFuCNp1oCs41NvCPRBuIBAqMCIDIbtsl0zxK\nKFF9NFgxAgMBAAECggEAA9rjdLB87zhIZnmwYtWwHJHJmasa0aLZbLDZ5HXY6skV\nBELvyzeshDfp06WS4GamisewVcdd3E/uk7IxWo7yi2y/agMRmy4AIakRcTfFl5cS\nTOFU3iMbCRWnW1mvqtwLdBO6ftnYcIAGkJA6wLaOd1sVIRAyf59karvhqr0rGET+\nf9mkk/cPSxClaeZ+D+6AyARBE0efX4Bxhr2rKmY8X1oOmfcKI3H8pj4XYBhKpFOO\nLCyAJ76q2g03uaMgzmNhWGUk6hHmH+YhiBhG8pUfgFA/JNfh13YX0VdIkCuNAXUk\nbfoXenzqVDcGpcMQyWiwFLFf6/hdN+nDi6r+sjNV7wKBgQD/zwIDxTbcsh9aMd8J\ngQnJ7X4lYQO567DLWR9iLcYf4nDcbQVMQPHHfHTO/SrNnMkgBPdkx4k/DC9IsU9E\nDqd5wos06UTXOrshab8DhN2VnLRTRp4KHDGKv8pVaTVaxNiNXjlh6Zc2Zw/ALqnb\nF447d+IjLYSc+s99QiabXKxDMwKBgQDXNdsVy7wzwSdEb0g2OnytYoxIls8DV3ph\na/e8ycnVXRk3M1/Cb/55FwNKEKwNHp2+lWrPVgfhRw9BrsxXmUzBHwO3zgGj8m2w\nOudEN706tGNAPKyVmJjTAMekcJk1O1gPjucEF8UFDiz1AjQ53xaDyGbgWDXsUj23\nrHp+6yy3CwKBgQDY2p7lnVi9zI5BgseFogsvUBJBE7X59XCBYgL10sIDI43zfsQf\n+Wo9ZycKlZmkhkYFy9ABchnKBcVLxWvh0N5unNovt5QxNMuQLrnL/OVvQFGpytKM\n1Yu2CJI4x6CAWyEM7pSMiV4zszip/aTmY1ii1yUwFxU1knU0xVA/Ilex+wKBgQCx\nxZMMFheIyO0e4q3L3dRPhTBMZgbqw8Ab6YvjLuzCivzhVFatzsVuUUwuSkdVLw0u\n+U4Zpv8rX/+CSloRolh8zpnLoyhHvWJOSNnwx/pQG+JAld4aVMsowWQoubpU9ohv\nkaPTBa+B1Yvdd8ycYWO/yDLy7urcI7NHnfoyDSIKsQKBgQCn0mAz/0FQCAF78PMM\nmpm247Hlm6eRYuM+gevb1QAo2JII10XeJVnsbLO25CMLHOwX7EpG8qN+uO8G379X\nRSCWtkf7u+90BzA5AFzsXcQqNKR8TsgUihgE6Gcb7fC+jsoQ3cBp3PhZFtteQHwJ\nMmBJVeq/IwYFSeh1LMDK0qJVSg==\n-----END PRIVATE KEY-----\n",
-  client_email: "firebase-adminsdk-fbsvc@caro-league.iam.gserviceaccount.com",
-  client_id: "111170103328278717336",
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40caro-league.iam.gserviceaccount.com",
-  universe_domain: "googleapis.com",
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
 };
